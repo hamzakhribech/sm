26516807b12d6b926ac2c09a8b75f2a860fa6544
hamzakhribech <hamzakhribech45@gmail.com>
2024-02-22 00:51:00 +0100

    phone and pc

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,11 @@ import "./Globalcss.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className='md:hidden'>
+       <App />
+    </div>
+    <div className='hidden md:block'>
+      <p className='text-xl font-bold text-center'>SCAN IT WITH YOUR PHONE</p>
+    </div>
   </React.StrictMode>,
 )
