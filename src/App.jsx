7a855bcf1b1e0271a9fc5898da961ea0b941ff4c
hamzakhribech <hamzakhribech45@gmail.com>
2024-02-22 00:19:23 +0100

    img fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import SMedia from "./sMedias";
 import Typewriter from "./typing.jsx";
 import Countdown from "./counter.jsx";
 export default function App() {
- const bg="url(/public/bgg.png)"
  
   return (
     <>
       {/* <p className="bg-red-500 text-5xl">hi</p> */}
       <div className="w-full h-full flex flex-col"
-      style={{
-        backgroundImage: bg,
-         backgroundSize: "cover",
-      }}
+     id="bg"
       >
         <div className="down px-3 py-1 flex justify-center items-center">
           <div className="w-[80%] pt-4 px-4 pb-2 bg-[#d3dbeb]   flex flex-col justify-center items-center gap-4 rounded-xl">
@@ -22,12 +18,12 @@ export default function App() {
         </div>
         <div className="cnt flex justify-center" >
         <div className="transform translate-y-[-50px] " >
-          <img className="transform scale-[1.2]" src="/public/title.png"/>
+          <img className="transform scale-[1.2]" src="/src/assets/title.png"/>
         </div>
         </div>
       </div>
       <div className="transform translate-y-[-138px]  flex flex-col gap-5" >
-          <img className="drop-shadow-xl drop-shadow-black" src="/public/date.png"/>
+          <img className="drop-shadow-xl drop-shadow-black" src="/src/assets/date.png"/>
        
       </div>
       <div className="transform translate-y-[-170px]  flex flex-col gap-5">
