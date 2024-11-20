8367b6833b886b7410fa1d1ef129a8733bd18ea4
Hamza Kribech <123521256+hamzakhribech@users.noreply.github.com>
2024-11-20 17:48:14 +0100

    Update loader.jsx

--- src/loader.jsx
+++ src/loader.jsx
@@ -3,7 +3,7 @@ const Loader = () => {
     return (
       <div className="pt-[200px] bg-white fixed w-screen h-screen z-[100]">
         <div className="h-[10px] flex justify-center">
-          <img  className="w-[100px] h-[100px]" src={logo} alt="Loading" />
+          <img  className="w-[100px] h-[100px]" src={logo} alt="" />
         </div>
    <div className="terminal-loader">
   <div className="terminal-header">
