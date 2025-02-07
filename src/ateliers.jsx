3e42a443bdecc19150a558fc86f15a4e7aeed549
SABDELS7 <salmiabdderrahmane@gmail.com>
2025-02-07 23:58:27 +0100

    2025 journee informatique

--- src/ateliers.jsx
+++ src/ateliers.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-irregular-whitespace */
-import poo from "/src/assets/poo.png"
-import userf from "/src/assets/userf.png"
-import five from "/src/assets/IconMMC/5.png"
-import six from "/src/assets/IconMMC/6.png"
+// import poo from "/src/assets/poo.png"
+import one from "/src/assets/or.png"
+import three from "/src/assets/IconMMC/3.png"
+import seven from "/src/assets/IconMMC/7.png"
 import eight from "/src/assets/IconMMC/8.png"
 import nine from "/src/assets/IconMMC/9.png"
+
 
 import { useEffect } from "react";
 const Ateliers = ({state}) => {
@@ -28,20 +29,20 @@ const Ateliers = ({state}) => {
        <div className="flex justify-start p-0 m-0  bg-gradient-to-b from-slate-50 to-slate-200">
       <div className=" text-center">
         <header className="p-0 m-0">
-          <h1 className="text-3xl p-0 m-0 bg-blue-300 shadow-md">Microsoft Technologies</h1>
+          <h1 className="text-3xl p-0 m-0 bg-blue-300 shadow-md">Ateliers & Workshops</h1>
         </header>
         <section className="block-body pt-5 ">
           <div className="grid grid-cols-1 md:grid-cols-2 ">
             <div className="at col-12 md:col-span-1 delay-300" data-visual="false">
               <div className="flex flex-wrap justify-center items-center">
-                <img className="w-[200px]" src={poo} />
+                <img className="w-[200px]" src={nine} />
                 <div className="flex flex-col transform -translate-y-5 ">
-                  <p className="text-xl font-semibold pt-3 m-0">.Net</p>
+                  <p className="text-xl font-semibold pt-3 m-0">Neural Networks</p>
                   <p className="text-gray-600 px-2">
                       {/* désigne une session de travail ou un ensemble d'activités pratiques visant à enseigner, explorer ou approfondir les concepts et les techniques liés à la programmation
                       orientée objet. Au cours de cet atelier, les participants peuvent se familiariser avec les principes fondamentaux de la POO tels que l'encapsulation, l'héritage, et le
                       polymorphisme, et ils ont souvent l'occasion de mettre en pratique ces concepts à travers des exercices de programmation concrets. */}
-                      .NET est un framework de Microsoft qui permet de créer des applications web, de bureau et mobiles avec une grande flexibilité. Il supporte plusieurs langages, comme C# et F#, et grâce à .NET Core, il est maintenant multiplateforme, fonctionnant sur Windows, macOS et Linux.
+                      Cet atelier vise à démystifier les réseaux de neurones, une composante cruciale de l'intelligence artificielle. À l'aide d'un modèle visuel, les participants découvriront comment ces réseaux apprennent et prennent des décisions. L'atelier abordera également des exemples concrets d'applications des réseaux de neurones dans divers domaines.
                   </p>
                 </div>
               </div>
@@ -50,66 +51,51 @@ const Ateliers = ({state}) => {
             
             <div className="atb col-12 md:col-span-1 delay-400" data-visual="false">
               <div className="flex flex-wrap justify-center items-center">
-                <img className="w-[200px]" src={six} />
+                <img className="w-[200px]" src={three} />
                 <div className="flex flex-col transform -translate-y-5 ">
-                  <p className="text-xl font-semibold pt-3  m-0">Microsoft Cloud Azure</p>
+                  <p className="text-xl font-semibold pt-3  m-0">Keys To Modern Software Development</p>
                   <p className="text-gray-600 px-3">
                       {/* cet atelier propose une expérience immersive axée sur l'apprentissage pratique des compétences liées au développement web. Les participants explorent divers domaines
                       tels que la création de sites, le développement front-end et back-end, ainsi que l'utilisation de technologies comme HTML, CSS, JavaScript et divers frameworks. À travers des exercices,
                       projets, discussions et démonstrations, l'atelier vise à approfondir la compréhension des participants des principes et des outils essentiels du développement web. */}
-                      Microsoft Azure est une plateforme cloud complète qui propose divers services pour créer, déployer et gérer des applications via des centres de données globaux. Elle permet aux entreprises de bénéficier d'infrastructures flexibles pour le stockage, l'analyse de données, le développement d'applications et bien plus, avec une forte intégration d'outils de sécurité et de machine learning.
-                  </p>
+                      Cet atelier présente quatre concepts fondamentaux du développement logiciel : l'expérience utilisateur (UX), la méthodologie agile, le contrôle de version (version control) et les tests/assurance qualité. À travers une interface interactive, les participants peuvent choisir l'un de ces concepts pour en apprendre davantage et découvrir comment il contribue à la création de logiciels de qualité.                  </p>
                 </div>
               </div>
               </div>
               <div className="at col-12 md:col-span-1 delay-400" data-visual="false">
               <div className="flex flex-wrap justify-center items-center">
-                <img className="w-[200px]" src={nine} />
+                <img className="w-[200px]" src={eight} />
                 <div className="flex flex-col transform -translate-y-5 ">
-                  <p className="text-xl font-semibold pt-3 m-0">Github Copilot</p>
+                  <p className="text-xl font-semibold pt-3 m-0">AI For Developers</p>
                   <p className="text-gray-600">
                     {/* partager avec les étudiants des informations, des conseils et des expériences pratiques sur la filière afin de les aider à prendre des décisions éclairées concernant leur orientation académique ou professionnelle. L'objectif est d'aider les étudiants  à mieux comprendre la filière,leur intérêts, leur compétences, et leur aspirations, tout en les familiarisant avec les opportunités disponibles. */}
-                    GitHub Copilot est un outil d'intelligence artificielle développé par GitHub et OpenAI pour aider les développeurs à écrire du code plus efficacement. Intégré directement dans des éditeurs comme Visual Studio Code, Copilot suggère des lignes de code, des fonctions complètes, et même des blocs entiers basés sur le contexte de ce que l'utilisateur est en train de coder. Il utilise des modèles d'apprentissage automatique pour anticiper les besoins du développeur et offrir des solutions, accélérant ainsi le processus de développement et facilitant l’apprentissage de nouvelles syntaxes ou technologies.
-                  </p>
+                    Cet atelier explore les avantages de l'intelligence artificielle pour les développeurs. Il montrera comment l'IA peut améliorer la qualité du code et faciliter la collaboration. L'atelier soulignera que l'IA est un outil au service des développeurs, et non une menace pour leur emploi.                  </p>
                 </div>
               </div>
               </div>
                <div className="atb col-12 md:col-span-1 delay-400" data-visual="false">
               <div className="flex flex-wrap justify-center items-center">
-                <img className="w-[200px]" src={eight} />
+                <img className="w-[200px]" src={seven} />
                 <div className="flex flex-col transform -translate-y-5 ">
-                  <p className="text-xl font-semibold pt-3 m-0">ML.Net</p>
+                  <p className="text-xl font-semibold pt-3 m-0">Software Development Lifecycle</p>
                   <p className="text-gray-600">
                      {/* L'objectif principal de cet atelier est généralement d'identifier les points forts et les faiblesses d'une interface ou d'un produit du point de vue de l'utilisateur. Les participants peuvent discuter des aspects tels que la navigation, la clarté des informations, la facilité d'utilisation des fonctionnalités, et d'autres éléments qui contribuent à une expérience positive.  */}
-                     ML.NET est une bibliothèque open-source de Microsoft qui permet aux développeurs .NET d’intégrer des modèles de machine learning dans leurs applications sans nécessiter de connaissances approfondies en data science. Elle prend en charge des tâches variées, telles que la classification, la régression, la détection d'anomalies et le clustering, et permet d'entraîner des modèles directement en C#. ML.NET propose également des outils comme Model Builder, une interface graphique dans Visual Studio, qui simplifie encore le processus de création de modèles prédictifs pour les développeurs.
-                  </p>
+                     Cet atelier présente les différentes étapes du processus de développement logiciel, de la conception à la maintenance. À travers des exemples concrets (applications web et mobiles), les participants découvriront comment chaque étape contribue à la création de logiciels de qualité. L'atelier abordera également les différents modèles de cycle de vie et les défis du développement logiciel.                  </p>
                 </div>
               </div>
               </div>
-              <div className="at col-12 md:col-span-1 delay-400" data-visual="false">
+               <div className="atb col-12 md:col-span-1 delay-400" data-visual="false">
               <div className="flex flex-wrap justify-center items-center">
-                <img className="w-[200px]" src={five} />
+                <img className="w-[200px]" src={one} />
                 <div className="flex flex-col transform -translate-y-5 ">
-                  <p className="text-xl font-semibold pt-3 m-0">Azure Devops</p>
+                  <p className="text-xl font-semibold pt-3 m-0">Ask A Student: Navigating The Engineering World</p>
                   <p className="text-gray-600">
-                   {/* vise à créer une visibilité, à établir des relations avec les étudiants et entreprises(connaître entreprises, avoir des contact,opportunités de stages...) et à promouvoir les produits ou services de l'entreprise lors d'événements  */}
-                   Azure DevOps est une plateforme de services cloud fournie par Microsoft pour le développement, l'intégration, et le déploiement continus des applications. Elle propose des outils de gestion de projets, de suivi des tâches, de contrôle de versions avec Git, et de pipelines CI/CD pour automatiser les tests et les déploiements. Azure DevOps aide les équipes à collaborer efficacement tout en optimisant le cycle de vie de développement logiciel.
-                  </p>
-                </div>
+                     {/* L'objectif principal de cet atelier est généralement d'identifier les points forts et les faiblesses d'une interface ou d'un produit du point de vue de l'utilisateur. Les participants peuvent discuter des aspects tels que la navigation, la clarté des informations, la facilité d'utilisation des fonctionnalités, et d'autres éléments qui contribuent à une expérience positive.  */}
+                     Parlons-en ! : <br></br>
+                     Venez échanger avec des étudiants en génie informatique. Posez toutes vos questions sur les études, les compétences recherchées, les projets étudiants, le quotidien d'un ingénieur en informatique et bien plus encore. </p>                </div>
               </div>
               </div>
-              <div className="atb col-12 md:col-span-1 delay-400" data-visual="false">
-              <div className="flex flex-wrap justify-center items-center">
-                <img className="w-[200px]" src={userf} />
-                <div className="flex flex-col transform -translate-y-5 ">
-                  <p className="text-xl font-semibold pt-3 m-0">Power BI</p>
-                  <p className="text-gray-600">
-                   {/* vise à créer une visibilité, à établir des relations avec les étudiants et entreprises(connaître entreprises, avoir des contact,opportunités de stages...) et à promouvoir les produits ou services de l'entreprise lors d'événements  */}
-                   Power BI est un outil de visualisation de données et de business intelligence permettant de transformer des données brutes en insights clairs et exploitables. Il offre des fonctionnalités de connexion aux sources de données, de création de rapports interactifs, et de partage de tableaux de bord. Power BI aide les entreprises à prendre des décisions basées sur les données, en rendant l'analyse accessible à tous les niveaux de l'organisation.
-                  </p>
-                </div>
-              </div>
-              </div>
+              
               
           </div>
         </section>
